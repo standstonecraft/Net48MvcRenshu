c95578fba2ffc91859f17c3acb974ecff1aa7aeb
standstonecraft <stand.stone.complex+github@gmail.com>
2021-07-01 02:03:09 +0900

    file upload

---
+++ Net48MvcRenshu/Controllers/UploadFilesController.cs
@@ -0,0 +1,67 @@
+﻿using Net48MvcRenshu.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Net48MvcRenshu.Controllers
+{
+    public class UploadFilesController : Controller
+    {
+        private static string folder = @"C:\develop\workspace\Net48MvcRenshu\folderForUploadFiles";
+        [HttpGet]
+        public ActionResult Index()
+        {
+            UploadFilesModel model = new UploadFilesModel();
+
+            var storedFile = Directory.GetFiles(folder).Select(f => new { name = Path.GetFileName(f), fileId = Server.UrlEncode(Path.GetFileName(f)) }).ToList();
+            model.StoredFileJson = JsonConvert.SerializeObject(storedFile);
+            return View("UploadFiles", model);
+        }
+
+        [HttpGet]
+        public FileResult Download(string fileId)
+        {
+            string actualName = fileId; // fileIdからファイル名に変換(実際は何か複雑な処理)
+            string actualPath = Path.Combine(folder, actualName);
+            return File(actualPath, MimeMapping.GetMimeMapping(actualPath), actualName);
+        }
+        [HttpPost]
+        public ActionResult Update(UploadFilesModel model)
+        {
+            // xボタンで削除されたファイルを実際に削除する
+            if (model.FileIdToRemove != null)
+            {
+                List<string> filesToRemove = model.FileIdToRemove.Split(',')
+                    .Select(fid =>
+                    {
+                        // fileId から物理ファイル名に変換する処理
+                        string actualPath = Path.Combine(folder, fid);
+                        return actualPath;
+                    }).ToList();
+                foreach (var file in filesToRemove)
+                {
+                    try
+                    {
+                        System.IO.File.Delete(file);
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                }
+            }
+
+            // アップロードされたファイルを保存する
+            List<HttpPostedFileWrapper> files = model.File.Where(f => f != null).ToList();
+            foreach (var file in files)
+            {
+                file.SaveAs(Path.Combine(folder, file.FileName));
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Net48MvcRenshu/Models/UploadFilesModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Net48MvcRenshu.Models
+{
+    public class UploadFilesModel
+    {
+        public HttpPostedFileWrapper[] File { get; set; }
+
+        public string StoredFileJson { get; set; }
+
+        public string FileIdToRemove { get; set; }
+    }
+}
